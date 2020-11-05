f5e7d570ad856fd3a81aa4f28505e16f3beeab97
Louis136 <louis.gnolfi@isen.yncrea.fr>
2020-11-05 19:49:05 +0100

    mail fini

--- mails/mailController.js
+++ mails/mailController.js
@@ -21,9 +21,27 @@ router.post("/sendmail",function (req, res) {
             console.log('pas erreur');
             res.json(rows)
         } 
-    })
+    });
+  
 
- 
+  });
+
+  
+router.post("/contactmail",function (req, res) {
+
+    mail.contactMail(req, function(err, rows) {
+
+        if(err) {
+            res.status(400).json(err);
+            console.log("erreur", err);
+        }
+        else{
+            console.log('pas erreur');
+            res.json(rows)
+        } 
+    });
+  
+
   });
   
   module.exports = router;
---
+++ test.js
@@ -0,0 +1,8 @@
+const help = require('./helpers/functions');
+
+let a = new Date();
+let b;
+b = help.dateNow(a);
+
+console.log(b);
+
