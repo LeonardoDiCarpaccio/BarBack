4e6cf048197a09a15be0cf84fd75eae5f2a9cbbd
maxpvp31 <loubet.maxime31@gmail.com>
2021-03-15 23:37:28 +0100

    quiz isen

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -15,10 +15,16 @@ var Items = {
             }
 
             if (table.length > 0)
-                return db.query('SELECT ' + target + ' FROM resultat WHERE ' + table.join(" AND "), callback);
-            else
-                return db.query('SELECT ' + target + ' FROM resultat', callback);
-        } else {
+{                return db.query('SELECT ' + target + ' FROM resultat WHERE ' + table.join(" AND "), function(err,rows){
+var res = Object.values(JSON.parse(JSON.stringify(rows)))
+callback(null,res)
+});
+}            else
+{                return db.query('SELECT ' + target + ' FROM resultat', function(err,rows){
+    var res = Object.values(JSON.parse(JSON.stringify(rows)))
+    callback(null,res)
+});
+}        } else {
             return callback('ERROR PARAMETERS');
         }
     },
