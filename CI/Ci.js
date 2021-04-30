d71a4ce3f4bdbe14b07389783ec4b12529e9c846
maxpvp31 <loubet.maxime31@gmail.com>
2021-04-30 13:19:31 +0200

    d

--- CI/Ci.js
+++ CI/Ci.js
@@ -10,7 +10,7 @@ const Category = {
 
         exec("cd .. & git pull origin main ", (error, stdout, stderr) => {
        
-            console.log("woihou")
+            console.log("ça work")
             
                 shell.exec('cd .. & sudo pm2 restart server.js', function(code, output) {
                    return callback(null,output)
