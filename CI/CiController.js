733f3c3050cb951e69eaac0c9b8d01023d3868ba
maxpvp31 <loubet.maxime31@gmail.com>
2021-04-30 12:44:34 +0200

    l

---
+++ CI/CiController.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+var Category = require('./Ci')
+
+
+router.get("/script", function(req, res){
+    Category.get(req,function(err , rows){
+        if(err){
+            res.status(400).json(err)
+        }
+        else{
+            res.json(rows)
+        }
+    })
+})
+
+
+module.exports = router
