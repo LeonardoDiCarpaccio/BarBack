bddd90519791d16a69bece3c7e4df430ebee9971
maxpvp31 <loubet.maxime31@gmail.com>
2021-04-30 13:12:26 +0200

    e

--- CI/Ci.js
+++ CI/Ci.js
@@ -6,16 +6,15 @@ var shell = require('shelljs');
 const Category = {
     //GET
     get: function (req, callback) {
-       
+    
 
         exec("cd .. & git pull origin main ", (error, stdout, stderr) => {
-        
-            if(stdout){
+       
+            
                 shell.exec('pm2 restart server.js', function(code, output) {
-                    console.log('Exit code:', code);
-                    console.log('Program output:', output);
+                   return callback(null,output)
                   });
-            }
+            
        
         });
 
