fae4ac3efa0107fdba0390a795488b8adc73fdbd
LeonardoDiCarpaccio <59809554+LeonardoDiCarpaccio@users.noreply.github.com>
2021-05-18 21:01:20 +0200

    lcuien

--- SocketServer/socketRoute/socketRouteController.js
+++ SocketServer/socketRoute/socketRouteController.js
@@ -30,7 +30,8 @@ router.post("/", function(req, res){
 router.post("/getOrderbyStatus", function(req, res){
     Socket.getOrderbyStatus(req,function(err , rows){
         if(err){
- res.status(400).json(err)        }
+ res.status(400).json(err)        
+    }
         else{
             res.json(rows)
             console.log('socket get info route')
