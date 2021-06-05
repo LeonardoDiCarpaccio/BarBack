8c8e1e333162c03ea58b03240c2d97e56d758cb9
maxpvp31 <loubet.maxime31@gmail.com>
2021-06-05 16:23:54 +0200

    e

--- util.test.js
+++ util.test.js
@@ -5,7 +5,7 @@ test('should return same string',()=>{
     const text = cleanQuery("test");
     expect(text).toBe("test")
 
-})
+});
 
 // test("test on real route",()=>{
 //     const a = Users.get({id_user : 1},function(err,rows){if(err){console.log(err) }else return rows})
