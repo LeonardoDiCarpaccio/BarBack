aa0ac3ac6a0388e6457e797daa8440144b36f509
maxpvp31 <loubet.maxime31@gmail.com>
2021-06-05 13:15:05 +0200

    j

---
+++ util.test.js
@@ -0,0 +1,14 @@
+const {cleanQuery} = require("./helpers/functions");
+const Users = require("./users/Users");
+
+// test('should return same string',()=>{
+//     const text = cleanQuery("test");
+//     expect(text).toBe("teerst")
+
+// })
+
+test("test on real route",()=>{
+    const a = Users.get({id_user : 1},function(err,rows){if(err){console.log(err) }else return rows})
+    console.log(a)
+    expect(a).toBe(Array)
+})
